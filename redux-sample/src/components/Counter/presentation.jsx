ea91f0cf2610de0cc67864d4c4032343dbb638ee
sumajirou <kougetsuin@gmail.com>
2020-08-23 21:28:11 +0900

    Counterと増減ボタンの追加

--- redux-sample/src/components/Counter/presentation.jsx
+++ redux-sample/src/components/Counter/presentation.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Counter = () => <div>counter</div>;
+const Counter = ({ count, increment, decrement }) => (
+  <>
+    <div>{count}</div>
+    <button onClick={() => increment(1)}>+1</button>
+    <button onClick={() => increment(10)}>+10</button>
+    <br />
+    <button onClick={() => decrement(1)}>-1</button>
+    <button onClick={() => decrement(10)}>-10</button>
+  </>
+);
 
 export default Counter;
